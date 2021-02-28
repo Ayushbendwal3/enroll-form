341cfd8d7d52fa9103372ea970d52a114b2bbe51
Ayush Bendwal <ayush.bendwal@nagarro.com>
2021-02-28 10:04:44 +0530

    almost done

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ const onSubmit = () => {
     }
   }
 
-  data.push(...data, {
+  data.push({
     name: name,
     email: email,
     website: website,
@@ -29,5 +29,33 @@ const onSubmit = () => {
     skills: [...skills]
   });
 
+  addRow(data);
+};
+
+const addRow = (data) => {
   console.log(data);
+  let table = document.getElementById('table-data');
+  let rowCount = table.rows.length;
+  let row = table.insertRow(rowCount);
+
+  row.insertCell(0).innerHTML = `<td>
+    <span class="font-weight-bold">${data[0].name}</span><br />
+    <span>${data[0].gender}</span><br />
+    <span>${data[0].email}</span><br />
+    <a
+      href="https://${data[0].website}"
+      target="_blank"
+      rel="noopener noreferrer"
+      ><u>${data[0].website}</u></a
+    ><br />
+    ${data[0].skills.map((skill) => {
+      return `<span>${skill}</span>`;
+    })}
+  </td>`;
+
+  row.insertCell(1).innerHTML = `<td style="width:100px;height:120px">
+                      <img src="${data[0].image}" 
+                      onerror="this.src='assets/fallback-image.jpg'"
+                      alt="image"/>
+                  </td>`;
 };
